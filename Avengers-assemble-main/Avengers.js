b31130e86922eed0fcb25f4249f0558e92469a90
Fruitiosh <82085768+Fruitiosh@users.noreply.github.com>
2021-04-15 17:59:41 +0530

    Add files via upload

---
+++ Avengers-assemble-main/Avengers.js
@@ -0,0 +1,46 @@
+if(keypressed == '70')
+{
+    new_image('ironman_face.png');
+    console.log("f");
+}
+
+if(keypressed == '66');
+{
+    new_image('spiderman_body.png');
+    console.log("b");
+}
+if(keypressed == '76')
+{
+    new_image('hulk_legs.png');
+    console.log("l");
+}
+if(keypressed == '82')
+{
+    new_image('thor_right_hang.png');
+    console.log("r");
+}
+if(keypressed == '72')
+{
+    new_image('captain_america_left_hand');
+    console.log("h");
+}
+if(keypressed == '38')
+{
+    up();
+    console.log("up");
+}
+if(keypressed == '40')
+{
+    down();
+    console.log("down");
+}
+if(keypressed == '37')
+{
+    left();
+    console.log("left");
+}
+if(keypressed == '39')
+{
+    right();
+    console.log("right");
+}
